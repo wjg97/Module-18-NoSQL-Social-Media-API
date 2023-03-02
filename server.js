05f144c354c33ead2b85fc01989f288a39eb7819
wjg97 <113846649+wjg97@users.noreply.github.com>
2023-03-02 09:24:38 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 //require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:Sheba1776@cluster0.f1agiti.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-API',{
+    //|| 'mongodb+srv://admin:Sheba1776@cluster0.f1agiti.mongodb.net/?retryWrites=true&w=majority', {
     useUnifiedTopology: true,  
     //useFindAndModify: false,
     useNewUrlParser: true,
@@ -22,7 +23,7 @@ mongoose.connection.once('open', function(){
     console.log('Connection Error:')
 });
 
-mongoose.set('debug', true);
+mongoose.set('debug', true); 
 
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
